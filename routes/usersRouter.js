2842b05f0f8c3934cdf338982178ddf297968fa6
Javier Fong <fong.kayyuen@gmail.com>
2020-08-13 22:25:28 +0800

    edit router

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -7,7 +7,7 @@ const UsersController = require('../controllers/usersController');
 router.post('/register', UsersController.register);
 router.post('/login', UsersController.login);
 router.put('/update/:id', ensureAuthenticated, UsersController.updateUser);
-router.get('/user', ensureAuthenticated, UsersController.getUser);
+router.get('/user', UsersController.getUser);
 router.get('/logout', UsersController.logout);
 router.get('/user/:username', ensureAuthenticated, UsersController.getUserbyUsername);
 
